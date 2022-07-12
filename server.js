b006800980f01a3932b6b79fd11de07cb5bd2c03
MrGultekin <gultekin.de@gmail.com>
2022-07-12 11:34:55 +0200

    third one

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { JobSource } from './models/JobSource.js';
 import express from "express";
+
+
+ dotenv.config();
+
+ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mrg-jobmanager';
+ mongoose.connect(MONGODB_URI)
 
 const app = express();
 const port = process.env.PORT || 3045;
