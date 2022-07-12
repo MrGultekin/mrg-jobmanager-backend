b90f0ec5f7c86a0692113ecf0e4f3c5cdc09e958
MrGultekin <gultekin.de@gmail.com>
2022-07-12 13:42:53 +0200

    online db works

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ import express from "express";
 
  dotenv.config();
 
- const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mrg-jobmanager';
- mongoose.connect(MONGODB_URI)
+ const MONGODB_URI =process.env.MONGODB_URI || 'mongodb://localhost/mrg-jobmanager';
+mongoose.connect(MONGODB_URI, (err) => {
+    if (err) {
+        console.log({
+            error: "Cannot connect to MongoDB database.",
+            err: `"${err}"`
+        });
+    }
+});
 
 const app = express();
 const port = process.env.PORT || 3045;
@@ -15,6 +22,12 @@ const port = process.env.PORT || 3045;
 app.get("/", (req, res) => {
   res.send("<h1>Job Manager API</h1>");
 });
+
+app.get('/job-sources',async (req, res) => {
+    const jobSources = await JobSource.find()
+    res.status(200).json(jobSources);
+})
+
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
