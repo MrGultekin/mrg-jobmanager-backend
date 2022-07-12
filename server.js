cf43e70d23e96bfbf0b208feb6c7e75b05c2918f
MrGultekin <gultekin.de@gmail.com>
2022-07-12 14:09:59 +0200

    empty

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import { JobSource } from './models/JobSource.js';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { JobSource } from "./models/JobSource.js";
 import express from "express";
 
+dotenv.config();
 
- dotenv.config();
-
- const MONGODB_URI =process.env.MONGODB_URI || 'mongodb://localhost/mrg-jobmanager';
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mrg-jobmanager";
 mongoose.connect(MONGODB_URI, (err) => {
-    if (err) {
-        console.log({
-            error: "Cannot connect to MongoDB database.",
-            err: `"${err}"`
-        });
-    }
+  if (err) {
+    console.log({
+      error: "Cannot connect to MongoDB database.",
+      err: `"${err}"`,
+    });
+  }
 });
 
 const app = express();
@@ -23,11 +23,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Job Manager API</h1>");
 });
 
-app.get('/job-sources',async (req, res) => {
-    const jobSources = await JobSource.find()
-    res.status(200).json(jobSources);
-})
-
+app.get("/job-sources", async (req, res) => {
+  const jobSources = await JobSource.find();
+  res.status(200).json(jobSources);
+});
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
